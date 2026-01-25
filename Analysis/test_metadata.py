858b2c2ee40a652e2fba3dcb22f35bfc492f14f9
Bryan Keating <ru.bryankeating@gmail.com>
2026-01-25 15:21:06 -0500

    Testing MetaData class in Docker

---
+++ Analysis/test_metadata.py
@@ -0,0 +1,15 @@
+from Ingestion.download_pgn import download_pgn
+from Ingestion.metadata import MetaData
+
+# USERNAME
+username = 'bkchessmaster2'
+trunc_usr = username.lower()[:8] # Truncate to 8 characters for filename
+
+# Download PGN games for user 'bkchessmaster2' from Jan 2026 onwards
+download_pgn(username, start_date='2026-01')
+
+# Extract metadata from PGN file
+bk_meta = MetaData(f'Data/Bronze/{trunc_usr}.pgn')
+
+# Save metadata to CSV
+bk_meta.save_csv(f'Data/Silver/{trunc_usr}_metadata.csv')
