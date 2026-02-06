3fb8f747f39d28f42bc50da509d8ef2ba4dd7c18
Bryan Keating <ru.bryankeating@gmail.com>
2026-02-06 17:44:28 -0500

    Familiarizing with time controls

--- Analysis/stak1_2025_analysis.py
+++ Analysis/stak1_2025_analysis.py
@@ -10,3 +10,20 @@ game_data = pd.read_csv(f'Data/Gold/{trun_usr}_game_data_gold.csv')
 # Display summary statistics of the game data
 print("\n📊 Game Data Summary:")
 print(game_data.describe())
+
+# Display column headers and data types
+print("\n📋 Game Data Columns and Types:")
+print(game_data.dtypes)
+
+# Print unique time control values
+print("\n⏱️ Unique Time Controls:")
+print(game_data['TimeControl'].unique())
+
+# # Curious
+# print("\n Unique Dates")
+# print(
+#     game_data[game_data['TimeControl'].isin(['1/86400', '1/259200'])]['UTCDate']
+# )
+
+# Describe games by time control
+# print(game_data[game_data['TimeControl'] == BLANK].describe())``
