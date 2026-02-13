4718366c8c45faa6e55e57a5e0a3f55573d096f3
Bryan Keating <ru.bryankeating@gmail.com>
2026-02-12 21:24:25 -0500

    Beginning sculpting Game table schema

--- Database/models.py
+++ Database/models.py
@@ -1,13 +1,15 @@
 from sqlalchemy import Column, Integer, String, ForeignKey, Date, DateTime
 from sqlalchemy.orm import relationship
-from datetime import datetime
+from datetime import datetime, UTC
 
 from base import Base
 
 class Player(Base):
-    __tablename__ = "players"
+    __tablename__ = "player"
 
     id = Column(Integer, primary_key=True)
     username = Column(String, unique=True, nullable=False)
-    
+    created_at = Column(DateTime, default=datetime.now(UTC))
 
+class Game(Base):
+    __tablename__ = "game"
