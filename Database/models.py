53327b503c87fc81e8ebbecb0fae97df98f7d2ab
Bryan Keating <ru.bryankeating@gmail.com>
2026-02-14 11:24:24 -0500

    Player and Game tables first draft
    Working on Move class after

--- Database/models.py
+++ Database/models.py
@@ -11,15 +11,42 @@ class Player(Base):
     username = Column(String, unique=True, nullable=False)
     created_at = Column(DateTime, default=datetime.now(UTC))
 
+    games = relationship("Game", back_populates="player")
+
 class Game(Base):
     __tablename__ = "game"
 
     id = Column(Integer, primary_key=True)
+    game_id = Column(Integer, nullable=False)
+
     player_id = Column(Integer, ForeignKey("player.id"),nullable=False)
+    player = relationship("Player", back_populates="games")
 
+    # Game Metadata
+    event = Column(String)
+    site = Column(String)
+    date = Column(String)
+    round = Column(String)
+    white = Column(String)
+    black = Column(String)
+    result = Column(String)
+    time_control = Column(String)
 
+    # Ratings
+    white_elo = Column(Integer)
+    black_elo = Column(Integer)
+    elo_diff = Column(Integer)
 
-    player = relationship("Player", back_populates="games") # player.games returns a list of all games
-    
+    # DateTime
+    utc_date = Column(String)
+    utc_time = Column(String)
+    start_time = Column(String)
+    end_date = Column(String)
+    end_time = Column(String)
+    timezone = Column(String)
+
+    # Move Data
+    ply = Column(Integer)
+    color = Column(Integer)
 
     
