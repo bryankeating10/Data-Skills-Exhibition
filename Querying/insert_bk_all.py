0538018a4cdcce027a38ac8e5a10c6944a3afdca
Bryan Keating <ru.bryankeating@gmail.com>
2026-02-15 19:38:45 -0500

    Add player to Player table function

--- Querying/insert_bk_all.py
+++ Querying/insert_bk_all.py
@@ -11,3 +11,21 @@ PROJECT_ROOT = Path(__file__).resolve().parents[1]
 METADATA_PATH = PROJECT_ROOT / "Data" / "Gold" / "bkchessm_metadata.csv"
 MOVEDATA_PATH = PROJECT_ROOT / "Data" / "Gold" / "bkchessm_moves_gold.csv"
 
+def create_player(session, username:str):
+    player = session.query(Player).filter_by(username=username).first()
+
+    if not player:
+        player = Player(username=username)
+        session.add(player)
+        session.flush()
+
+    return player
+
+def insert_games(session,metadata_df, player_id):
+    games = []
+
+    for _, row in metadata_df.iterrows():
+        game = Game(
+            game_id=row['game_id'],
+            player_id=player_id
+        )
