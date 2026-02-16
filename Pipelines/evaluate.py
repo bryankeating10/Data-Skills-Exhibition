b0b8597dd6a8411a08114ab31156d87bce107fca
Bryan Keating <ru.bryankeating@gmail.com>
2026-02-16 18:54:08 -0500

    Docstring

---
+++ Pipelines/evaluate.py
@@ -0,0 +1,18 @@
+"""
+Title:
+    Docstring for Pipelines.evaluate
+
+Description:
+    Function for evaluation of move data, backing up the 
+    results in /Data
+
+Usage:
+    evaluate(move_df)
+        or
+    evaluate(move_df, depth==25)
+
+Output:
+    Dataframe containing evaluated positions
+"""
+
+# 
