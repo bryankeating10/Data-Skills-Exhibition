17e75a0ac313c31ffad88185addb6bebb8837ca8
Bryan Keating <ru.bryankeating@gmail.com>
2026-02-15 19:33:23 -0500

    Static path variables

--- Querying/insert_bk_all.py
+++ Querying/insert_bk_all.py
@@ -1,6 +1,13 @@
-import os
+# Dependencies
+from pathlib import Path
 import pandas as pd
 
+# Database interaction modules
 from Database.session import SessionLocal
 from Database.models import Player, Game, Move
+
+# Static path variables
+PROJECT_ROOT = Path(__file__).resolve().parents[1]
+METADATA_PATH = PROJECT_ROOT / "Data" / "Gold" / "bkchessm_metadata.csv"
+MOVEDATA_PATH = PROJECT_ROOT / "Data" / "Gold" / "bkchessm_moves_gold.csv"
 
