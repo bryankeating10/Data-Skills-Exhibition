6ac597c918f9b41dc4b23c87dc6de483bdd001d9
Bryan Keating <ru.bryankeating@gmail.com>
2026-02-16 17:08:05 -0500

    Docstring output

--- Pipelines/ingest.py
+++ Pipelines/ingest.py
@@ -3,11 +3,16 @@ Title:
     Docstring for Pipelines.ingest
 
 Description:
-    Download from Chess.com into MetaData and MoveData classes,
+    Function for download from Chess.com into MetaData and MoveData classes,
     backing up the information in /Data
 
 Usage:
     ingest('bkchessmaster2')
         or
     ingest('bkchessmaster2', start_date='2024-01', end_date='2024-12')
+
+Output:
+    Populated MetaData and MoveData classes
 """
+
+# 
