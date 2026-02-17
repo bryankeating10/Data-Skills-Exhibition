727b6bffb04338a48c8eb288336e72bfeb3cb792
Bryan Keating <ru.bryankeating@gmail.com>
2026-02-16 20:31:28 -0500

    Testing insertion to Postgres database

---
+++ Testing/test_insert.py
@@ -0,0 +1,14 @@
+from Pipelines.ingest import ingest
+from Pipelines.evaluate import evaluate
+from Pipelines.insert import insert
+
+# Chess.com username
+USERNAME = 'kingsk4'
+
+# Ingestion
+meta_df, move_df = ingest(USERNAME, start_date='2024-02', end_date='2024-02')
+
+# Evaluation
+eval_df = evaluate(move_df, depth=8, username=USERNAME)
+
+# Database insertion
