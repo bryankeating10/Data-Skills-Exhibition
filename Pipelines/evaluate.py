8fa71c10a2d1a35b3cb612eae7cbe15d3febd25f
Bryan Keating <ru.bryankeating@gmail.com>
2026-02-16 19:47:13 -0500

    Evaluate function

--- Pipelines/evaluate.py
+++ Pipelines/evaluate.py
@@ -16,6 +16,15 @@ Output:
 """
 
 # Evaluation modules
-from Processing.add_eval import add_eval, repopulate_unique_evals
+from Processing.add_eval import add_eval, repop
 from Processing.unique_fen import unique_fens
 
+def evaluate(move_df, depth=20):
+    # Extract unique FENs into a Series to avoid redundant evaluations
+    unique_fens = unique_fens(move_df)
+
+    # Evaluate unique FENs using Stockfish at desired depth
+    evaluated_fens = add_eval(unique_fens, depth=depth)
+
+    # Map evaluations back to the moves df
+    eval_df = repop(move_df, evaluated_fens)
