cda395492db08d45ec836f69a641044f7593d8f6
Bryan Keating <ru.bryankeating@gmail.com>
2026-02-16 20:53:31 -0500

    Docstring

---
+++ Database/insert_games.py
@@ -0,0 +1,14 @@
+"""
+Title:
+    Docstring for Database.insert_games
+
+Description:
+    Inserts a metadata dataframe into the Meta table in Postgres
+
+Usage:
+    from session import session
+    insert_games(session, metadata_df, player_id)
+
+Output:
+    None
+"""
