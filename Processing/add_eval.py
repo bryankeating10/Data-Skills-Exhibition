a919a5041f69ffbe2d349e04e5032281b3808c2f
Bryan Keating <ru.bryankeating@gmail.com>
2026-02-17 06:15:01 -0500

    Reformatting ETA display

--- Processing/add_eval.py
+++ Processing/add_eval.py
@@ -80,7 +80,7 @@ def add_eval(unique_fen_series: pd.Series, depth: int = 15) -> pd.Series:
 
         if (i + 1) % 25 == 0:
             print(f"Evaluated {i + 1}/{total} positions... ({invalid_count} invalid)")
-            print(f"Completion: {round((i+1)*100/total),2}%")
+            print(f"Completion: {round((i+1)*100/total,2)}%")
             print(f"ETA: {hours} hours and {minutes} minutes")
             
     
