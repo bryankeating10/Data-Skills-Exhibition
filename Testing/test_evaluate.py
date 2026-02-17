edf6f3eeb67a9ec47a89e1fc8725df5a664394a7
Bryan Keating <ru.bryankeating@gmail.com>
2026-02-16 22:39:39 -0500

    Processing my own data from the past two months on depth 25

--- Testing/test_evaluate.py
+++ Testing/test_evaluate.py
@@ -2,13 +2,13 @@ from Pipelines.ingest import ingest
 from Pipelines.evaluate import evaluate
 
 # USERNAME
-USERNAME = 'kingsk4'
+USERNAME = 'bkchessmaster2'
 _, move_df = ingest(USERNAME, start_date='2026-02', end_date='2026-02')
 
 print('PRE-EVALUATION')
 print(move_df.head(7))
 
-eval_df = evaluate(move_df, depth=8, username=USERNAME)
+eval_df = evaluate(move_df, depth=25, username=USERNAME)
 
 print('POST-EVALUATION')
 print(eval_df.head(7))
