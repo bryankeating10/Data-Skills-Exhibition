2d9e63027d9e9b74a3fe30bde72b0329453a7ff2
Bryan Keating <ru.bryankeating@gmail.com>
2026-02-16 19:59:18 -0500

    Parametrized username

--- Testing/test_evaluate.py
+++ Testing/test_evaluate.py
@@ -1,12 +1,14 @@
 from Pipelines.ingest import ingest
 from Pipelines.evaluate import evaluate
 
-_, move_df = ingest('kingsk4','2026-02','2026-02')
+# USERNAME
+USERNAME = 'kingsk4'
+_, move_df = ingest(USERNAME, start_date='2026-02', end_date='2026-02')
 
 print('PRE-EVALUATION')
 print(move_df.head(7))
 
-eval_df = evaluate(move_df)
+eval_df = evaluate(move_df, depth=8, username=USERNAME)
 
 print('POST-EVALUATION')
 print(eval_df.head(7))
