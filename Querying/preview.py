c7191777765475b8d86911d744a3bc497ddc33ea
Bryan Keating <ru.bryankeating@gmail.com>
2026-02-17 19:44:31 -0500

    Preview player function and main guard

--- Querying/preview.py
+++ Querying/preview.py
@@ -13,3 +13,19 @@ from sqlalchemy import select, func
 
 from Database.session import SessionLocal
 from Database.models import Player, Meta, Move
+
+def preview_players(session):
+    print("\n--- Players ---")
+
+    stmt = select(Player)
+    players = session.scalars(stmt).all()
+
+    for player in players:
+        print(
+            f"ID: {player.player_id} | "
+            f"Username: {player.username} | "
+            f"Created: {player.created_at}"
+        )
+
+if __name__ == '__main__':
+    main()
