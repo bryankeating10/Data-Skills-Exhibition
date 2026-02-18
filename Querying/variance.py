e8b2d0b9c401fb76069d2e3cd62f5f973c6945c0
Bryan Keating <ru.bryankeating@gmail.com>
2026-02-18 06:41:20 -0500

    Investigating variance by time control

---
+++ Querying/variance.py
@@ -0,0 +1,10 @@
+from sqlalchemy import select
+from Database.session import SessionLocal
+from Database.models import Meta
+
+def unique_time_controls():
+    with SessionLocal as session:
+        stmt = select(Meta.time_control).distinct()
+        result = session.execute(stmt)
+        return result.scalars().all()
+    
