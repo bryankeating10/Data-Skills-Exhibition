e9ce23452c2d9550cdc8e9f435e481e31b350f7b
Bryan Keating <ru.bryankeating@gmail.com>
2026-02-20 06:46:52 -0500

    docstring

---
+++ Pipelines/derive.py
@@ -0,0 +1,14 @@
+"""
+Title:
+    Pipelines.derive Docstring
+
+Description:
+    Populates the MetaDerived and MoveDerived tables with data from Meta 
+    and Move for more manageable and meaningful queries down the road
+
+Usage:
+    from Pipelines.derive import derive
+    derive()
+Output:
+
+"""
